dcee6e473154b2dd70f6711d5dc41d6339bc13fa
Pedro <pedro_hasse@yahoo.com.br>
2022-07-25 21:09:42 -0300

    :sparkles: Requisito 2 ok

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,33 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 export class Form extends Component {
   render() {
-    // const {
-    //   cardName,
-    //   cardDescription,
-    //   cardAttr1, cardAttr2,
-    //   cardAttr3,
-    //   cardImage,
-    //   cardRare,
-    //   cardTrunfo,
-    //   hasTrunfo,
-    //   isSaveButtonDisabled,
-    //   onInputChange,
-    //   onSaveButtonClick
-    // } = this.props;
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1, cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+      // hasTrunfo,
+      isSaveButtonDisabled,
+      onInputChange,
+      onSaveButtonClick,
+    } = this.props;
     return (
       <form>
         <label htmlFor="card-name">
           Card Name:
-          <input type="text" name="card-name" id="card-name" data-testid="name-input" />
+          <input
+            type="text"
+            name="card-name"
+            id="card-name"
+            data-testid="name-input"
+            value={ cardName }
+            onChange={ onInputChange }
+          />
         </label>
         <label htmlFor="card-desc">
           Card Description:
@@ -30,6 +37,8 @@ export class Form extends Component {
             cols="30"
             rows="10"
             data-testid="description-input"
+            value={ cardDescription }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="card-attr1">
@@ -39,6 +48,8 @@ export class Form extends Component {
             name="card-attr1"
             id="card-attr1"
             data-testid="attr1-input"
+            value={ cardAttr1 }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="card-attr2">
@@ -48,6 +59,8 @@ export class Form extends Component {
             name="card-attr2"
             id="card-attr2"
             data-testid="attr2-input"
+            value={ cardAttr2 }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="card-attr3">
@@ -57,6 +70,8 @@ export class Form extends Component {
             name="card-attr3"
             id="card-attr3"
             data-testid="attr3-input"
+            value={ cardAttr3 }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="card-image">
@@ -66,43 +81,62 @@ export class Form extends Component {
             name="card-image"
             id="card-image"
             data-testid="image-input"
+            value={ cardImage }
+            onChange={ onInputChange }
           />
         </label>
         <label htmlFor="card-rarity">
           Card Rarity:
-          <select name="card-rarity" id="card-rarity" data-testid="rare-input">
+          <select
+            name="card-rarity"
+            id="card-rarity"
+            data-testid="rare-input"
+            value={ cardRare }
+            onChange={ onInputChange }
+          >
             <option value="normal">Normal</option>
             <option value="raro">Raro</option>
             <option value="muito raro">Muito Raro</option>
           </select>
         </label>
         <label htmlFor="super-trunfo">
-          Super-Trunfo?
+          Super Trunfo?
           <input
             type="checkbox"
             name="super-trunfo"
             id="super-trunfo"
+            checked
             data-testid="trunfo-input"
+            value={ cardTrunfo }
+            onChange={ onInputChange }
           />
         </label>
-        <button type="submit" data-testid="save-button">Salvar</button>
+        <button
+          type="submit"
+          data-testid="save-button"
+          disabled={ isSaveButtonDisabled }
+          onClick={ onSaveButtonClick }
+        >
+          Salvar
+        </button>
       </form>
     );
   }
 }
 
-// Form.propTypes = {
-//   cardName: PropTypes.string.isRequired,
-//   cardDescription: PropTypes.string.isRequired,
-//   cardAttr1: PropTypes.string.isRequired,
-//   cardAttr2: PropTypes.string.isRequired,
-//   cardAttr3: PropTypes.string.isRequired,
-//   cardImage: PropTypes.string.isRequired,
-//   cardRare: PropTypes.string.isRequired,
-//   cardTrunfo: PropTypes.bool,
-//   hasTrunfo: PropTypes.bool,
-//   isSaveButtonDisabled: PropTypes.bool,
-//   onInputChange: PropTypes.
-// };
+Form.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+  // hasTrunfo: PropTypes.bool.isRequired,
+  isSaveButtonDisabled: PropTypes.bool.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  onSaveButtonClick: PropTypes.func.isRequired,
+};
 
 export default Form;
